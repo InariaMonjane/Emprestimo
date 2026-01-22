2fd286fb2a6756a01e83752f4501f161d5d8dbcf
apple <apple@apples-MacBook-Pro.local>
2026-01-22 20:02:53 +0800

    Add JAWSDB support

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('CLEARDB_DATABASE_URL') ?: env('JAWSDB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -59,8 +59,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+    PDO::MYSQL_ATTR_SSL_CA ?? null,
+]) : [],
         ],
 
         'pgsql' => [
